ded41187eb73f08ea0cb067cc0f44164147a229c
vinicius2896 <141687544+vinicius2896@users.noreply.github.com>
2024-06-07 19:49:41 -0300

    dog api atualizado

--- app/dogs/page.js
+++ app/dogs/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import axios from "axios";
 import { useState, useEffect } from "react";
@@ -7,40 +7,42 @@ import "./style.css";
 export default function Dogs() {
     const [racas, alteraRacas] = useState([]);
     const [imagens, alteraImagens] = useState([]);
-    const [nome, alteraNome] = useState([]);
-    
+    const [nome, alteraNome] = useState('');
 
     function buscarRacas() {
         axios.get("https://dog.ceo/api/breeds/list/all")
             .then(function(response) {
                 alteraRacas(Object.keys(response.data.message));
+            })
+            .catch(function(error) {
+                console.error("Erro ao buscar raças: ", error);
             });
     }
 
-    function buscarImagens(racaCachorro) {
-
-        alteraNome (racaCachorro)
-
-        let url = "http://dog.ceo/api/breed/"+racaCachorro+"/images";
-
-        if(racaCachorro == null){
-            url = "https://dog.ceo/api/breeds/image/random/20";
+    function buscarImagens(racaCachorro = null) {
+        if (racaCachorro) {
+            alteraNome(racaCachorro);
+        } else {
+            alteraNome('Todas as raças');
         }
+
+        let url = racaCachorro 
+            ? `https://dog.ceo/api/breed/${racaCachorro}/images`
+            : "https://dog.ceo/api/breeds/image/random/20";
 
         axios.get(url)
             .then(function(response) {
                 alteraImagens(response.data.message);
+            })
+            .catch(function(error) {
+                console.error("Erro ao buscar imagens: ", error);
             });
-        }
+    }
 
-    
- 
-
-    useEffect( ()=> {
+    useEffect(() => {
         buscarRacas();
         buscarImagens();
     }, []);
-
 
     return (
         <div id="dog">
@@ -51,14 +53,12 @@ export default function Dogs() {
 
             <div className="layout-central">
                 { /* Menu com a lista das raças para clicar */ }
-                <div className="layout-menu" >
-                    
-                    <li onClick={() => buscarImagens()}>Todas as raças</li>
-                    
+                <div className="layout-menu">
+                    <li key="todas" onClick={() => buscarImagens()}>Todas as raças</li>
                     <ul>
                         { 
                             racas.map(r => (
-                                <li key={r} onClick={() => bucarImagens(r)}>
+                                <li key={r} onClick={() => buscarImagens(r)}>
                                     {r}
                                 </li>
                             ))
@@ -68,9 +68,9 @@ export default function Dogs() {
 
                 { /* Painel de fotos dos cachorros */ }
                 <div>
-                <h2> {nome}</h2>
+                    <h2>{nome}</h2>
                     {imagens.map((imagem, index) => (
-                        <img key={index} src={imagem} alt="Husky Dog" />
+                        <img key={index} src={imagem} alt="Dog" />
                     ))}
                 </div>
             </div>
